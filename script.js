3fd2561231edb0c5d03441d5c34faeba50ee5cc6
Martin Svejda <svejdam13@gmail.com>
2020-11-28 14:51:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game-container');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
+
+// List of words for game
+const words = [
+  'hate',
+  'peace',
+  'independent',
+  'suit',
+  'likeble',
+  'juice',
+  'warlike',
+  'sad',
+  'mute',
+  'dependent',
+  'steer',
+  'silver',
+  'highfalutin',
+  'superficial',
+  'canvas',
+  'seven',
+  'feeble',
+  'adorable',
+  'drag',
+  'loving'
+];
+
+
