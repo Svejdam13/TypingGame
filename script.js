3359a54c053fc84905a019e6f9a695b1827a361e
Martin Svejda <svejdam13@gmail.com>
2020-12-11 18:44:06 +0100

    script.js

--- script.js
+++ script.js
@@ -31,5 +31,40 @@ const words = [
   'drag',
   'loving'
 ];
+// Init word
+let randomWord;
+// Init score
+let score = 0;
+// Init time
+let time = 10;
 
+// Generate random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
 
+//Add word to DOM
+function addWordToDom() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord; 
+}
+// Update score
+function updateScore(){
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+addWordToDom();
+// Event listeners
+
+text.addEventListener('input', e => {
+  const insertedText = e.target.value;
+  
+  if(insertedText === randomWord) {
+    addWordToDom();
+    updateScore();
+
+    //Clear
+    e.target.value = '';
+  }
+});
